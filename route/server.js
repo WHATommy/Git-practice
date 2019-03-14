b521e414c5af8021072f6c9fbdb572d308f66cfa
WHATommy <tranman100598@gmail.com>
2019-03-13 18:55:28 -0700

    added new router for country

--- route/server.js
+++ route/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 router.get('/', (req, res) => {
     res.json({ status: 'success' });
+})
+
+router.get('/:country', (req, res) => {
+    axios
+        .get(`https://restcountries.eu/rest/v2/name/${req.body.name}`)
+        .then(country => req.json(country))
+        .catch(err => {
+            console.log(err)
+        })
 })
 
 router.post('/', (req, res) => {
